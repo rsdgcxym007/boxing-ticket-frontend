57b306f4aa7d045b7d5b67c62802d6282ade4f43
rsdgcxym007 <590107030026@dpu.ac.th>
2025-07-12 02:30:30 +0700

    #ifx:

--- utils/zoneEnums.ts
+++ utils/zoneEnums.ts
@@ -1,9 +1,9 @@
 export const ZONE_NAMES = {
-  "cdf12f66-e147-4df3-a295-586d93917ffc": "back-left",
-  "4cd004ed-625d-4ffc-a636-215f38d5d4ae": "back-right",
-  "7638ac7a-4f94-4330-9bb3-f233a1a62a09": "left",
-  "9c72f498-7b27-48ae-a7f4-86ec55b2c5c8": "right",
-  "88fd3f40-69df-4869-be08-c729cebdc10c": "front-ringside",
+  "61c2dea0-beb5-4a8d-b26d-6ca45ad3f194": "back-left",
+  "35cb87bd-9e7f-4c8a-8ce5-f233a981a97d": "back-right",
+  "f79cebc6-81aa-49b4-8e45-b81197760ded": "left",
+  "d5b3d095-c93a-4ce4-a601-482e9475f954": "right",
+  "ddb14cab-e1eb-42b6-b284-077081b48585": "front-ringside",
 } as const;
 
 export const ZONE_LABELS = {
@@ -15,9 +15,9 @@ export const ZONE_LABELS = {
 } as const;
 
 export const ZONE_IDS_BY_NAME = {
-  "back-left": "945b915e-89f1-40bc-b162-033a959b8e6d",
-  "back-right": "21015e2c-9140-479a-8a62-1338ef529cd2",
-  left: "0ec7de07-b545-476c-a662-8269095d906d",
-  right: "169d4228-5ab4-4150-b159-3147e5a51cae",
-  "front-ringside": "4208f8f5-7fc8-45c7-be4f-7b3fb5b5d1aa",
+  "back-left": "61c2dea0-beb5-4a8d-b26d-6ca45ad3f194",
+  "back-right": "35cb87bd-9e7f-4c8a-8ce5-f233a981a97d",
+  left: "f79cebc6-81aa-49b4-8e45-b81197760ded",
+  right: "d5b3d095-c93a-4ce4-a601-482e9475f954",
+  "front-ringside": "ddb14cab-e1eb-42b6-b284-077081b48585",
 } as const;
