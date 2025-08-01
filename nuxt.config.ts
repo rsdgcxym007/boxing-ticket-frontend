7fea34521f7293b3c8f7b9f6f896f93fedfb8b2a
rsdgcxym007 <590107030026@dpu.ac.th>
2025-08-01 23:42:34 +0700

    #fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,11 @@ export default defineNuxtConfig({
 
   devtools: { enabled: false },
   ssr: false,
+  nitro: {
+    prerender: {
+      routes: ['/']
+    }
+  },
 
   // Electron specific configuration
   router: {
