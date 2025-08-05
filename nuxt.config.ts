1fd7f7b59e5940d65db855587c1b3c32714eaf3a
rsdgcxym007 <590107030026@dpu.ac.th>
2025-08-05 16:29:49 +0700

    #fix:logo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -62,7 +62,7 @@ export default defineNuxtConfig({
         {
           rel: "icon",
           type: "image/x-icon",
-          href: "/favicon.ico",
+          href: "/images/logo/LOGOFC.svg",
         },
         {
           rel: "stylesheet",
